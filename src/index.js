9021e865db4508734fd215c6b75059f01b83472f
Eddie Haddican <edwardhaddican@gmail.com>
2022-04-04 14:44:55 -0400

    updated ReactDOM render to react 18 specs

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import { Main } from "./components";
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 //import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-      <Main />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById("root");
+
+// Create a root.
+const root = ReactDOM.createRoot(container);
+
+// Initial render
+root.render(<Main />);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//       <Main />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
